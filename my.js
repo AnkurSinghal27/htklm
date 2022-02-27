186c9b9ff269f8e30cc897a1f791dc69d3dc555c
AnkurSinghal27 <41937633+AnkurSinghal27@users.noreply.github.com>
2022-02-27 01:41:19 -0800

    Add files via upload

---
+++ my.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id= "demo"></p>
+<script>
+constant pi= 3.14;
+let person= "john";
+lrt answer= "yes";
+document.getElementById(demo).innerHTML= pi + "<br>" + person + "<br>" + answer ;
+</script>
+</body>
+</html>
